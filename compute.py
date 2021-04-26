a93571fb04cd704826d01a6900c41e9a90b44725
Stephen Foster <ess.gee.eff@gmail.com>
2021-04-26 15:29:30 -0300

    Implement limit functionality

--- compute.py
+++ compute.py
@@ -6,6 +6,8 @@ limit = float(sys.argv[2])
 total = 0.0
 for line in sys.stdin:
     output = max(0.0, float(line) - threshold)
+    if (total + output > limit):
+        output = limit - total
     print("{:.1f}".format(output))
     total += output
 
