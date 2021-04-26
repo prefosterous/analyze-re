332c239479a3a199e64a6c73630dcf1ee626ed0e
Stephen Foster <ess.gee.eff@gmail.com>
2021-04-26 15:08:01 -0300

    Apply threshold and print total count

--- compute.py
+++ compute.py
@@ -1,5 +1,12 @@
 import sys
 
+threshold = float(sys.argv[1])
+limit = float(sys.argv[2])
+
+total = 0.0
 for line in sys.stdin:
-    a = float(line)
-    print("{:.1f}".format(a))
+    output = max(0.0, float(line) - threshold)
+    print("{:.1f}".format(output))
+    total += output
+
+print(total)
