b6a36f1ef136c8458b3c5b6137de1cfd76ffbf33
Stephen Foster <ess.gee.eff@gmail.com>
2021-04-27 21:01:28 -0300

    Added basic error-handling, fixed output format for total

--- compute.py
+++ compute.py
@@ -1,14 +1,22 @@
 import sys
 
-threshold = float(sys.argv[1])
-limit = float(sys.argv[2])
+try:
+    threshold = float(sys.argv[1])
+    limit = float(sys.argv[2])
+except:
+    sys.exit("Invalid command line arguments. This script requires two numeric arguments as input.")
 
 total = 0.0
-for line in sys.stdin:
-    output = max(0.0, float(line) - threshold)
-    if (total + output > limit):
-        output = limit - total
-    print("{:.1f}".format(output))
-    total += output
 
-print(total)
+try:
+    for line in sys.stdin:
+        output = max(0.0, float(line) - threshold)
+        if (total + output > limit):
+            output = limit - total
+        print("{:.1f}".format(output))
+        total += output
+
+    print("{:.1f}".format(total))
+
+except:
+    sys.exit("An error occurred while reading or processing the data. Make sure your input file is formatted correctly.")
